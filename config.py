fa49276ddffa20bb38bd579fa47f6cb4609093fe
n019620-hue <n019620@gmail.com>
2026-02-15 21:47:00 +0530

    Update SQLALCHEMY_DATABASE_URI default path
    Change default SQLite database path to /tmp/database.db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'pr-tech-connect-secret-key-2026'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:////tmp/database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
     
