73e7f02694982d41d785f7e646ed3a59539bc04f
n019620-hue <n019620@gmail.com>
2026-02-15 23:08:39 +0530

    Refactor default admin user creation logic
    Refactor admin user creation logic to check for existence before creating.

--- app.py
+++ app.py
@@ -16,13 +16,12 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-    # Create default admin if not exists
-if not User.query.filter_by(username="admin").first():
+    admin = User.query.filter_by(username="admin").first()
+    if admin is None:
         default_user = User(username="admin")
         default_user.set_password("1234")
         db.session.add(default_user)
         db.session.commit()
-        print("Default admin created")
 
 # ---------------- LOGIN MANAGER ---------------- #
 login_manager = LoginManager()
